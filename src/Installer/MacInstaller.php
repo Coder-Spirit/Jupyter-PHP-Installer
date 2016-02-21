e75783a5b5d831b9cd73c85a092c114887e15302
Andreu Correa Casablanca <castarco@gmail.com>
2016-02-21 17:08:53 +0100

    Added OSX installer

--- src/Installer/MacInstaller.php
+++ src/Installer/MacInstaller.php
@@ -18,4 +18,40 @@ final class MacInstaller extends Installer
     {
         parent::__construct($system, $composerCmd);
     }
+
+    /**
+     * @return string
+     */
+    protected function getInstallPath()
+    {
+        $currentUser = $this->system->getCurrentUser();
+
+        if ('root' === $currentUser) {
+            return '/Applications/jupyter-php';
+        } else {
+            return $this->system->getCurrentUserHome().'/Library/jupyter-php';
+        }
+    }
+
+    /**
+     *
+     */
+    protected function installKernel()
+    {
+        $kernelDef = json_encode([
+            'argv' => ['php', $this->getInstallPath().'/pkgs/src/kernel.php', '{connection_file}'],
+            'display_name' => 'PHP',
+            'language' => 'php',
+            'env' => new \Stdclass
+        ]);
+
+        $currentUser = $this->system->getCurrentUser();
+
+        $kernelSpecPath = ('root' === $currentUser) ?
+            '/usr/local/share/jupyter/kernels/jupyter-php' :
+            $this->system->getCurrentUserHome().'/Library/Jupyter/kernels/jupyter-php';
+
+        $this->system->ensurePath($kernelSpecPath);
+        file_put_contents($kernelSpecPath.'/kernel.json', $kernelDef);
+    }
 }
